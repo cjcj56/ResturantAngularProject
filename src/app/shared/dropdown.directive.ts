43101f4bbc7eee7e77efe0eac133872c8ec88a66
cjcj56@gmail.com <cjcj56@gmail.com>
2019-07-15 19:32:53 +0300

    improve dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
@@ -6,10 +6,18 @@ import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@an
 export class DropdownDirective {
     @HostBinding('class.open') isOpen = false;
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+    // constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+    constructor(private elementRef: ElementRef) {}
 
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    // @HostListener('click') toggleOpen() {
+    //     if (this.isOpen) {
+    //         this.renderer.addClass(this.elementRef.nativeElement, 'open');
+    //     } else {
+    //         this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+    //     }
+    // }
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
         // if (this.isOpen) {
         //     this.renderer.addClass(this.elementRef.nativeElement, 'open');
         // } else {

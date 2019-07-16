d1ffc57c3150be97b60e92e123fdfd4e02ed7a34
cjcj56@gmail.com <cjcj56@gmail.com>
2019-07-16 17:47:01 +0300

    finish recipes form

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { RecipesService } from '../recipes/recipes.service';
+import { Recipe } from '../recipes/recipe.model';
+
+
+@Injectable({providedIn: 'root'})
+export class  DataStorageService {
+
+    private databaseUrl = 'https://resturant-demo-a9d8d.firebaseio.com';
+    private recipesUrl: string;
+    private shoppinListUrl: string;
+
+    constructor(private http: HttpClient, private recipesService: RecipesService) {
+        this.recipesUrl = this.databaseUrl + '/' + 'recipes.json';
+        this.shoppinListUrl = this.databaseUrl + '/' + 'shoppingList.json';
+    }
+
+
+    storeRecipes() {
+        const recipes = this.recipesService.getRecipes();
+        this.http.put(this.recipesUrl, recipes)
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>(this.recipesUrl)
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }))
+        .subscribe(recipes => {
+            this.recipesService.setRecipes(recipes);
+        });
+    }
+}
